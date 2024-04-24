0a9b6f7b8f1b176aede25091a3eb18f35714cd26
Guilherme Vinicius da Silva <guilherme.svinicius@gmail.com>
2024-04-23 23:03:02 -0300

    js-faq e js- url seguro e  bicicletas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar sublinhado Menu javascript
 const links = document.querySelectorAll('.header-menu a');
 function ativarLink(link){
     const url = location.href;
@@ -7,3 +8,31 @@ function ativarLink(link){
     }
 }
 links.forEach(ativarLink)
+
+//Ativas itens do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro);
+    if(elemento){
+        elemento.checked = true;
+    }
+}
+parametros.forEach(ativarProduto)
+
+//Perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+}
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener("click", ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas)
